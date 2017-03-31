cb43b9917c0b7aa8db672dba9da072b92973fabc
K550jk <951759534@qq.com>
2017-03-31 16:07:41 +0800

    day3

---
+++ 18.js
@@ -0,0 +1,17 @@
+/*
+*
+* 将函数 fn 的执行上下文改为 obj 对象
+ 输入例子:
+ speak(function () {return this.greeting + ', ' + this.name + '!!!';}, {greeting: 'Hello', name: 'Rebecca'})
+
+ 输出例子:
+ Hello, Rebecca!!!
+*
+*
+*
+*
+* */
+function speak(fn, obj) {
+    var str =  fn.call(obj);
+    return str;
+}
